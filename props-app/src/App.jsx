349b216d198233f03615e97f9bcf7772edb7c337
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-07-14 13:22:34 +0530

    props default and types

--- props-app/src/App.jsx
+++ props-app/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
       <Card id={1} name="Sreekesh" desc="Voluptate, omnis incidunt. Quidem fugit deserunt enim libero quae! Pariatur, dolorem corrupt" isStudent={true}/> 
       <Card id={2} name="Tactinton" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit." isStudent={false}/>
       <Card id={3} name="FreakSreek" desc="Cumque totam tenetur incidunt eligendi quaerat iure pariatur distinctio quasi, nam mollitia dolor est aut sequi culpa assumenda error accusamus, commodi tempora!" isStudent={false}/>
+      <Card></Card>
+      <Card></Card>
     </>
   )
 }
--- props-app/src/Card.jsx
+++ props-app/src/Card.jsx
@@ -1,12 +1,27 @@
-
+import PropTypes from 'prop-types'
 function Card(props) {
     return (
         <div className="card">
             <h1><small>{props.id}</small>.{props.name}</h1>
-            <h3>{props.isStudent ? "Student":"Not Student"}</h3>
+            <h3>{props.isStudent ? "Student" : "Not Student"}</h3>
             <p>{props.desc}</p>
 
         </div>
     );
 }
+
+Card.PropTypes ={
+    id: PropTypes.number,
+    name:PropTypes.string,
+    isStudent: PropTypes.bool
+}
+
+
+Card.defaultProps ={
+    id: 0,
+    name:"Unknown",
+    desc:"Null",
+    isStudent: false
+}
+
 export default Card
