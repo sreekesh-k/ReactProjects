cb1df751a44e3a7351868f9142f6147d96fc0320
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-07-16 10:43:13 +0530

    tailwind

--- parralax-demo/src/Parralax.jsx
+++ parralax-demo/src/Parralax.jsx
@@ -1,7 +1,15 @@
+import React, { useEffect } from 'react';
+import Parallax from 'parallax-js';
 
-function Parralax(){
-    return(
+function Parralax() {
+    useEffect(() => {
+        const scene = document.getElementById('scene');
+        new Parallax(scene);
+    }, [])
+    return (
         <>
+            <section id="scene" className="relative w-full h-full parallax-container overflow-hidden">
+            </section>
         </>
     );
 }
